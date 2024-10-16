a693314a6197444d32c57b2c61a8005b74025e50
msatriaprayoga <msatriaprayoga92@gmail.com>
2024-10-16 12:16:37 +0700

    first commit

---
+++ homework.py
@@ -0,0 +1,14 @@
+desa = str(input('Desa : '))
+no_kartu_keluarga = int(input('No Kartu Keluarga : '))
+nik = int(input('Nomer Induk Kependudukan : '))
+nama = str(input('Nama warga : '))
+tempat_lahir = str(input('Tempat Lahir : '))
+agama = str(input('Agama : '))
+umur = int(input('Umur : '))
+pekerjaan = str(input('Pekerjaan : '))
+gaji_perbulan = int(input('Gaji_perbulan : '))
+jumlah_anak = int(input('Jumlah anak : '))
+
+daftar_pekerjaan = ['Guru','Tni','Polisi']
+hasil = 'mendapatkan bantuan pkh' if (pekerjaan not in daftar_pekerjaan and gaji_perbulan < 3000000 and umur >= 25 and jumlah_anak >= 1) else 'tidak mendapatkan bantuan pkh'
+print(hasil)
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Bantuan PKH.
+
+# Sebuah bantuan PKH yang ingin dibagi kepada desa dan warganya agar tidak terjadi kericuhan antar warga.
+
+# Membuat data desa dan warganya dengan cara mengisi data diri dan kriteria untuk mendapatkan bantuan PKH.
+
+# FLOWCHART
+![flowchart]()
